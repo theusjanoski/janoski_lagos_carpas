d4ac572fa513f438141c7a3c2c971f94e910624a
MuriloAlvarez <murilo12alvarez@gmail.com.br>
2024-09-09 18:45:14 -0300

    test: ✅ Primeiro-commit

--- janoski_lagos/src/app/page.jsx
+++ janoski_lagos/src/app/page.jsx
@@ -14,13 +14,8 @@ export default function Home() {
         />
         <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
           <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              src/app/page.tsx
-            </code>
-            .
+            OI JANOSKI{" "}
           </li>
-          <li>Save and see your changes instantly.</li>
         </ol>
 
         <div className="flex gap-4 items-center flex-col sm:flex-row">
